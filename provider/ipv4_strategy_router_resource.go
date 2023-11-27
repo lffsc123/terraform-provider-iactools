8487ac5b582a42a80fac01e5bb14e5c9abc41632
lffsc123 <1032188134@qq.com>
2023-11-27 16:18:43 +0800

    update

--- provider/ipv4_strategy_router_resource.go
+++ provider/ipv4_strategy_router_resource.go
@@ -190,7 +190,7 @@ func (r *Ipv4StrategyRouterResource) Update(ctx context.Context, req resource.Up
 
 func (r *Ipv4StrategyRouterResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
 	var data *Ipv4StrategyRouterResourceModel
-	tflog.Info(ctx, " Delete Start ************* ")
+	tflog.Info(ctx, " Delete Start *************  ")
 
 	//sendToweb_Ipv4StrategyRouterRequest(ctx, "DELETE", r.client, data.AddIpv4StrategyRouterParameter)
 
