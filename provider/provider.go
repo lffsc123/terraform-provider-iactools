b0d8dc3a96065982701cbe53db08a4a1690a9320
lffsc123 <1032188134@qq.com>
2024-01-09 10:49:23 +0800

    init

--- provider/provider.go
+++ provider/provider.go
@@ -7,7 +7,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/provider"
 	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
-	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 )
 
@@ -21,10 +20,10 @@ type ScaffoldingProvider struct {
 
 // ScaffoldingProviderModel describes the provider data model.
 type ScaffoldingProviderModel struct {
-	Port     types.String `tfsdk:"port"`
-	Address  types.String `tfsdk:"address"`
-	Username types.String `tfsdk:"username"`
-	Password types.String `tfsdk:"password"`
+	//Port     types.String `tfsdk:"port"`
+	//Address  types.String `tfsdk:"address"`
+	//Username types.String `tfsdk:"username"`
+	//Password types.String `tfsdk:"password"`
 }
 
 func (p *ScaffoldingProvider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
@@ -36,94 +35,83 @@ func (p *ScaffoldingProvider) Schema(ctx context.Context, req provider.SchemaReq
 	tflog.Info(ctx, "Schema******** *****")
 	resp.Schema = schema.Schema{
 		Attributes: map[string]schema.Attribute{
-			"port": schema.StringAttribute{
-				MarkdownDescription: "  provider attribute",
-				Optional:            true,
-			}, "address": schema.StringAttribute{
-				MarkdownDescription: "  provider attribute",
-				Optional:            true,
-			}, "username": schema.StringAttribute{
-				MarkdownDescription: "  provider attribute",
-				Optional:            true,
-			}, "password": schema.StringAttribute{
-				MarkdownDescription: "  provider attribute",
-				Optional:            true,
-			},
+			//"port": schema.StringAttribute{
+			//	MarkdownDescription: "  provider attribute",
+			//	Optional:            true,
+			//}, "address": schema.StringAttribute{
+			//	MarkdownDescription: "  provider attribute",
+			//	Optional:            true,
+			//}, "username": schema.StringAttribute{
+			//	MarkdownDescription: "  provider attribute",
+			//	Optional:            true,
+			//}, "password": schema.StringAttribute{
+			//	MarkdownDescription: "  provider attribute",
+			//	Optional:            true,
+			//},
 		},
 	}
 }
 
 func (p *ScaffoldingProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
-	var data ScaffoldingProviderModel
-
-	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
-	tflog.Info(ctx, "Configure****+*******")
-	if resp.Diagnostics.HasError() {
-		return
-	}
-	address, port, name, password := "", "", "", ""
-	if !data.Address.IsNull() {
-		address = data.Address.ValueString()
-	}
-	if !data.Port.IsNull() {
-		port = data.Port.ValueString()
-	}
-	if !data.Username.IsNull() {
-		name = data.Username.ValueString()
-	}
-	if !data.Password.IsNull() {
-		password = data.Password.ValueString()
-	}
-
-	if data.Port.IsNull() {
-		tflog.Info(ctx, "Port is NULL")
-		return
-	}
-	if data.Address.IsNull() {
-		tflog.Info(ctx, "Address is NULL")
-		return
-	}
-	if data.Username.IsNull() {
-		tflog.Info(ctx, "Username is NULL")
-		return
-	}
-	if data.Password.IsNull() {
-		tflog.Info(ctx, "Password is NULL")
-		return
-	}
-	tflog.Info(ctx, address+port+name+password)
-	address = address + port
-
-	autha := AuthStruct{Username: name, Password: password}
-
-	client, err := NewClient(&address, &autha)
-	if err != nil {
-		resp.Diagnostics.AddError(
-			"Unable to Create HashiCups API Client",
-			"An unexpected error occurred when creating the HashiCups API client. "+
-				"If the error is not clear, please contact the provider developers.\n\n"+
-				"HashiCups Client Error: "+err.Error(),
-		)
-		return
-	}
-	resp.DataSourceData = client
-	resp.ResourceData = client
+	//var data ScaffoldingProviderModel
+	//
+	//resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+	//tflog.Info(ctx, "Configure****+*******")
+	//if resp.Diagnostics.HasError() {
+	//	return
+	//}
+	//address, port, name, password := "", "", "", ""
+	//if !data.Address.IsNull() {
+	//	address = data.Address.ValueString()
+	//}
+	//if !data.Port.IsNull() {
+	//	port = data.Port.ValueString()
+	//}
+	//if !data.Username.IsNull() {
+	//	name = data.Username.ValueString()
+	//}
+	//if !data.Password.IsNull() {
+	//	password = data.Password.ValueString()
+	//}
+	//
+	//if data.Port.IsNull() {
+	//	tflog.Info(ctx, "Port is NULL")
+	//	return
+	//}
+	//if data.Address.IsNull() {
+	//	tflog.Info(ctx, "Address is NULL")
+	//	return
+	//}
+	//if data.Username.IsNull() {
+	//	tflog.Info(ctx, "Username is NULL")
+	//	return
+	//}
+	//if data.Password.IsNull() {
+	//	tflog.Info(ctx, "Password is NULL")
+	//	return
+	//}
+	//tflog.Info(ctx, address+port+name+password)
+	//address = address + port
+	//
+	//autha := AuthStruct{Username: name, Password: password}
+	//
+	//client, err := NewClient(&address, &autha)
+	//if err != nil {
+	//	resp.Diagnostics.AddError(
+	//		"Unable to Create HashiCups API Client",
+	//		"An unexpected error occurred when creating the HashiCups API client. "+
+	//			"If the error is not clear, please contact the provider developers.\n\n"+
+	//			"HashiCups Client Error: "+err.Error(),
+	//	)
+	//	return
+	//}
+	//resp.DataSourceData = client
+	//resp.ResourceData = client
 }
 
 func (p *ScaffoldingProvider) Resources(ctx context.Context) []func() resource.Resource {
 	return []func() resource.Resource{
-		NewSourceNatResource,
-		NewTargetNatResource,
-		NewIpv4RouterResource,
-		NewIpv4StrategyRouterResource,
-		NewPfPolicyResource,
-		NewVsysResource,
-		NewSecurityZoneResource,
-		NewNetAddrObjResource,
-		NewNetAddrGroupResource,
-		NewUsrObjResource,
-		NewUsrGroupResource,
-		NewVrrpResource,
+		NewGeneralApiResource,
 	}
 }
 
--- provider/security_zone_resource.go
+++ provider/security_zone_resource.go
@@ -11,105 +11,58 @@ import (
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 )
 
-// 安全域
-var _ resource.Resource = &SecurityZoneResource{}
-var _ resource.ResourceWithImportState = &SecurityZoneResource{}
+// 通用api
+var _ resource.Resource = &GeneralApiResource{}
+var _ resource.ResourceWithImportState = &GeneralApiResource{}
 
-func NewSecurityZoneResource() resource.Resource {
-	return &SecurityZoneResource{}
+func NewGeneralApiResource() resource.Resource {
+	return &GeneralApiResource{}
 }
 
-type SecurityZoneResource struct {
+type GeneralApiResource struct {
 	client *Client
 }
 
-type SecurityZoneResourceModel struct {
-	AddSecurityZoneParameter AddSecurityZoneParameter `tfsdk:"securityzonelist"`
+type GeneralApiResourceModel struct {
+	AddGeneralApiParameter AddGeneralApiParameter `tfsdk:"generalapiparam"`
 }
 
-type AddSecurityZoneRequest struct {
-	AddSecurityZoneRequestModel AddSecurityZoneRequestModel `json:"securityzonelist"`
-}
-
-type UpdateSecurityZoneRequest struct {
-	UpdateSecurityZoneRequestModel UpdateSecurityZoneRequestModel `json:"securityzonelist"`
+type AddGeneralApiRequest struct {
+	AddGeneralApiRequestModel AddGeneralApiRequestModel `json:"generalapiparam"`
 }
 
 // 调用接口参数
-type AddSecurityZoneRequestModel struct {
-	Vsysname    string `json:"vsysName"`
-	Name        string `json:"name"`
-	Priority    string `json:"priority"`
-	Interfaces  string `json:"interfaces"`
-	Desc        string `json:"desc"`
-	Inneraction string `json:"innerAction"`
-}
-
-type UpdateSecurityZoneRequestModel struct {
-	Vsysname    string `json:"vsysName"`
-	Name        string `json:"name"`
-	OldName     string `json:"oldName"`
-	Priority    string `json:"priority"`
-	Interfaces  string `json:"interfaces"`
-	Desc        string `json:"desc"`
-	Inneraction string `json:"innerAction"`
+type AddGeneralApiRequestModel struct {
+	Url         string `json:"url"`
+	Method      string `json:"method"`
+	RequestBody string `json:"requestBody"`
 }
 
 // 接收外部参数
-type AddSecurityZoneParameter struct {
-	Vsysname    types.String `tfsdk:"vsysname"`
-	Name        types.String `tfsdk:"name"`
-	Priority    types.String `tfsdk:"priority"`
-	Interfaces  types.String `tfsdk:"interfaces"`
-	Desc        types.String `tfsdk:"desc"`
-	Inneraction types.String `tfsdk:"inneraction"`
+type AddGeneralApiParameter struct {
+	Url         types.String `tfsdk:"url"`
+	Method      types.String `tfsdk:"method"`
+	RequestBody types.String `tfsdk:"requestBody"`
 }
 
-// 查询结果结构体
-type QuerySecurityZoneResponseListModel struct {
-	Securityzonelist []QuerySecurityZoneResponseModel `json:"securityzonelist"`
-}
-type QuerySecurityZoneResponseModel struct {
-	Vsysname     string `json:"vsysName"`
-	Name         string `json:"name"`
-	Priority     string `json:"priority"`
-	Interfaces   string `json:"interfaces"`
-	Desc         string `json:"desc"`
-	Inneraction  string `json:"innerAction"`
-	OldName      string `json:"oldName"`
-	ReferNum     string `json:"referNum"`
-	DelAllEnable string `json:"delAllEnable"`
-	Offset       string `json:"offset"`
-	Count        string `json:"count"`
+func (r *GeneralApiResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = "dpfirewall_GeneralApi"
 }
 
-func (r *SecurityZoneResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
-	resp.TypeName = "dpfirewall_SecurityZone"
-}
-
-func (r *SecurityZoneResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+func (r *GeneralApiResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
 	resp.Schema = schema.Schema{
 		Attributes: map[string]schema.Attribute{
-			"securityzonelist": schema.SingleNestedAttribute{
+			"generalapiparam": schema.SingleNestedAttribute{
 				Required: true,
 				Attributes: map[string]schema.Attribute{
-					"vsysname": schema.StringAttribute{
+					"url": schema.StringAttribute{
 						Optional: true,
 					},
-					"name": schema.StringAttribute{
+					"method": schema.StringAttribute{
 						Required: true,
 					},
-					"priority": schema.StringAttribute{
+					"requestBody": schema.StringAttribute{
 						Required: true,
-					},
-					"interfaces": schema.StringAttribute{
-						Optional: true,
-					},
-					"desc": schema.StringAttribute{
-						Optional: true,
-					},
-					"inneraction": schema.StringAttribute{
-						Optional: true,
 					},
 				},
 			},
@@ -117,7 +70,7 @@ func (r *SecurityZoneResource) Schema(ctx context.Context, req resource.SchemaRe
 	}
 }
 
-func (r *SecurityZoneResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+func (r *GeneralApiResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
 	if req.ProviderData == nil {
 		return
 	}
@@ -137,46 +90,46 @@ func (r *SecurityZoneResource) Configure(ctx context.Context, req resource.Confi
 	r.client = client
 }
 
-func (r *SecurityZoneResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
-	var data *SecurityZoneResourceModel
+func (r *GeneralApiResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var data *GeneralApiResourceModel
 	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
 	if resp.Diagnostics.HasError() {
 		return
 	}
 	tflog.Trace(ctx, "created a resource ************** ")
-	sendToweb_SecurityZoneRequest(ctx, "POST", r.client, data.AddSecurityZoneParameter)
+	sendToweb_GeneralApiRequest(ctx, r.client, data.AddGeneralApiParameter)
 	// Save data into Terraform state
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
 
-func (r *SecurityZoneResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
-	var data *SecurityZoneResourceModel
+func (r *GeneralApiResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	var data *GeneralApiResourceModel
 	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
 
 	if resp.Diagnostics.HasError() {
 		return
 	}
 	tflog.Info(ctx, " read Start ***************")
-	//sendToweb_SecurityZoneRequest(ctx, "GET", r.client, data.AddSecurityZoneParameter)
+	//sendToweb_GeneralApiRequest(ctx, "GET", r.client, data.AddGeneralApiParameter)
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
 
-func (r *SecurityZoneResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
-	var data *SecurityZoneResourceModel
+func (r *GeneralApiResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var data *GeneralApiResourceModel
 	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
 	if resp.Diagnostics.HasError() {
 		return
 	}
 	tflog.Info(ctx, " Update Start ************")
-	//sendToweb_SecurityZoneRequest(ctx, "PUT", r.client, data.AddSecurityZoneParameter)
+	//sendToweb_GeneralApiRequest(ctx, "PUT", r.client, data.AddGeneralApiParameter)
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
 
-func (r *SecurityZoneResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
-	var data *SecurityZoneResourceModel
+func (r *GeneralApiResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var data *GeneralApiResourceModel
 	tflog.Info(ctx, " Delete Start *************")
 
-	//sendToweb_SecurityZoneRequest(ctx, "DELETE", r.client, data.AddSecurityZoneParameter)
+	//sendToweb_GeneralApiRequest(ctx, "DELETE", r.client, data.AddGeneralApiParameter)
 
 	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
 
@@ -185,68 +138,25 @@ func (r *SecurityZoneResource) Delete(ctx context.Context, req resource.DeleteRe
 	}
 }
 
-func (r *SecurityZoneResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+func (r *GeneralApiResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
 	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
 }
 
-func sendToweb_SecurityZoneRequest(ctx context.Context, reqmethod string, c *Client, Rsinfo AddSecurityZoneParameter) {
-
-	if reqmethod == "POST" {
-		// 先查询是否存在，再执行新增操作
-		tflog.Info(ctx, "安全域--开始执行--查询操作")
-		responseBody := sendRequest(ctx, "GET", c, nil, "/func/web_main/api/security_zone/security_zone/securityzonelist?vsysName=PublicSystem&offset=0&count=1000", "安全域")
-		var queryResList QuerySecurityZoneResponseListModel
-		err := json.Unmarshal([]byte(responseBody), &queryResList)
-		if err != nil {
-			panic("转换查询结果json出现异常")
-		}
-		for _, queryRes := range queryResList.Securityzonelist {
-			if queryRes.Name == Rsinfo.Name.ValueString() {
-				tflog.Info(ctx, "安全域--存在重复数据，执行--修改操作")
-				var sendUpdateData UpdateSecurityZoneRequestModel
-				sendUpdateData = UpdateSecurityZoneRequestModel{
-					Vsysname:    Rsinfo.Vsysname.ValueString(),
-					Name:        Rsinfo.Name.ValueString(),
-					OldName:     Rsinfo.Name.ValueString(),
-					Priority:    Rsinfo.Priority.ValueString(),
-					Interfaces:  Rsinfo.Interfaces.ValueString(),
-					Desc:        Rsinfo.Desc.ValueString(),
-					Inneraction: Rsinfo.Inneraction.ValueString(),
-				}
-
-				requstUpdateData := UpdateSecurityZoneRequest{
-					UpdateSecurityZoneRequestModel: sendUpdateData,
-				}
-				body, _ := json.Marshal(requstUpdateData)
-
-				sendRequest(ctx, "PUT", c, body, "/func/web_main/api/security_zone/security_zone/securityzonelist", "安全域")
-				return
-			}
-		}
-
-		// 新增操作
-		var sendData AddSecurityZoneRequestModel
-		sendData = AddSecurityZoneRequestModel{
-			Vsysname:    Rsinfo.Vsysname.ValueString(),
-			Name:        Rsinfo.Name.ValueString(),
-			Priority:    Rsinfo.Priority.ValueString(),
-			Interfaces:  Rsinfo.Interfaces.ValueString(),
-			Desc:        Rsinfo.Desc.ValueString(),
-			Inneraction: Rsinfo.Inneraction.ValueString(),
-		}
-
-		requstData := AddSecurityZoneRequest{
-			AddSecurityZoneRequestModel: sendData,
-		}
-		body, _ := json.Marshal(requstData)
-
-		sendRequest(ctx, reqmethod, c, body, "/func/web_main/api/security_zone/security_zone/securityzonelist", "安全域")
-		return
-	} else if reqmethod == "GET" {
-
-	} else if reqmethod == "PUT" {
-
-	} else if reqmethod == "DELETE" {
-
+func sendToweb_GeneralApiRequest(ctx context.Context, c *Client, Rsinfo AddGeneralApiParameter) {
+	// 新增操作
+	var sendData AddGeneralApiRequestModel
+	sendData = AddGeneralApiRequestModel{
+		RequestBody: Rsinfo.RequestBody.ValueString(),
 	}
+
+	requstData := AddGeneralApiRequest{
+		AddGeneralApiRequestModel: sendData,
+	}
+	body, _ := json.Marshal(requstData)
+	tflog.Info(ctx, "===请求url==="+Rsinfo.Url.ValueString()+"===")
+	tflog.Info(ctx, "===请求方法==="+Rsinfo.Method.ValueString()+"===")
+	tflog.Info(ctx, "===请求参数转换前==="+Rsinfo.RequestBody.ValueString()+"===")
+	tflog.Info(ctx, "===请求参数转换后==="+string(body)+"===")
+	//sendRequest(ctx, Rsinfo.Method.ValueString(), c, body, Rsinfo.Url.ValueString(), "通用api")
+	return
 }
